cc86a5d1a013c780656662b021ec6993c66571ea
AdaHe <168495114+aAdaHe@users.noreply.github.com>
2025-04-04 18:27:03 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,15 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+      <!-- 在 HTML 中嵌入 PHP 代碼，這會顯示 "Hello world!" -->
+        <?php echo "Hello world!"; ?> 
+      <!-- 顯示 HTML 的標題，這是純 HTML，不會嵌入 PHP -->
+        <h1>html</h1>
+      <!-- 再次使用 PHP 顯示 "Hello world!" -->
+        <?php echo "Hello world!"; ?> 
+      <!-- 顯示第二個 HTML 標題 -->
+        <h1>html</h1>
+      <!-- 再次使用 PHP 顯示 "Hello world!" -->
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>

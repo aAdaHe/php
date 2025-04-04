278bf11ff4d740f42ee0eee3c127fbd2ad3b0494
AdaHe <168495114+aAdaHe@users.noreply.github.com>
2025-04-04 18:51:29 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"];   # 輸出用戶提交的 id 資料
+    echo $_POST["id"];   # 輸出提交的 id 資料
     echo "<br>";  # 換行
-    echo $_POST["pwd"];   # 輸出用戶提交的 pwd 資料
+    echo $_POST["pwd"];   # 輸出提交的 pwd 資料
 ?>
